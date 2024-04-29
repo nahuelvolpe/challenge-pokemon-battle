fcde40311a9ea83a28996ef65275501a9c4fab7e
Nahuel Volpe <vnahui@outlook.com>
2024-04-29 00:23:37 -0300

    se agrega hook useBattle

---
+++ frontend/src/hooks/useBattle.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export const useBattle = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [rivalPokemon, setRivalPokemon] = useState(null);
+  const [winnerPokemon, setWinnerPokemon] = useState(null);
+  const [openNotification, setOpenNotification] = useState(false);
+
+  const fetchPokemons = async () => {
+    const response = await axios.get("http://localhost:3000/pokemon");
+    setPokemons(response?.data);
+  };
+
+  const battleHandle = async () => {
+    if (selectedPokemon && rivalPokemon) {
+      const body = {
+        pokemonSelected: selectedPokemon,
+        pokemonRival: rivalPokemon,
+      };
+      const response = await axios.post("http://localhost:3000/battle", body);
+      setWinnerPokemon(response?.data);
+    }
+  };
+
+  const getRivalPokemon = () => {
+    const availablePokemons = pokemons.filter(
+      (pokemon) => pokemon.id !== selectedPokemon.id
+    );
+
+    const randomIndex = Math.floor(Math.random() * availablePokemons.length);
+
+    return availablePokemons[randomIndex];
+  };
+
+  useEffect(() => {
+    if (selectedPokemon) {
+      setRivalPokemon(getRivalPokemon());
+    }
+  }, [selectedPokemon]);
+
+  useEffect(() => {
+    if (winnerPokemon) {
+      setOpenNotification(true);
+    }
+  }, [winnerPokemon]);
+
+  return {
+    pokemons,
+    selectedPokemon,
+    rivalPokemon,
+    winnerPokemon,
+    openNotification,
+    setSelectedPokemon,
+    setOpenNotification,
+    fetchPokemons,
+    battleHandle,
+  };
+};
--- frontend/src/pages/PokemonPage.jsx
+++ frontend/src/pages/PokemonPage.jsx
@@ -4,55 +4,24 @@ import { Grid, Typography } from "@mui/material";
 import { ListPokemon } from "../components/ListPokemon";
 import { BattleComponent } from "../components/BattleComponent";
 import { NotificationComponent } from "../components/NotificationComponent";
+import { useBattle } from "../hooks/useBattle";
 
 export const PokemonPage = () => {
-  const [pokemons, setPokemons] = useState([]);
-  const [selectedPokemon, setSelectedPokemon] = useState(null);
-  const [rivalPokemon, setRivalPokemon] = useState(null);
-  const [winnerPokemon, setWinnerPokemon] = useState(null);
-  const [openNotification, setOpenNotification] = useState(false);
-
-  const fetchPokemons = async () => {
-    const response = await axios.get("http://localhost:3000/pokemon");
-    setPokemons(response?.data);
-  };
-
-  const battleHandle = async () => {
-    if (selectedPokemon && rivalPokemon) {
-      const body = {
-        pokemonSelected: selectedPokemon,
-        pokemonRival: rivalPokemon,
-      };
-      const response = await axios.post("http://localhost:3000/battle", body);
-      setWinnerPokemon(response?.data);
-    }
-  };
-
-  const getRivalPokemon = () => {
-    const availablePokemons = pokemons.filter(
-      (pokemon) => pokemon.id !== selectedPokemon.id
-    );
-
-    const randomIndex = Math.floor(Math.random() * availablePokemons.length);
-
-    return availablePokemons[randomIndex];
-  };
+  const {
+    pokemons,
+    selectedPokemon,
+    rivalPokemon,
+    winnerPokemon,
+    openNotification,
+    setSelectedPokemon,
+    setOpenNotification,
+    fetchPokemons,
+    battleHandle,
+  } = useBattle();
 
   useEffect(() => {
     fetchPokemons();
   }, []);
-
-  useEffect(() => {
-    if (winnerPokemon) {
-      setOpenNotification(true);
-    }
-  }, [winnerPokemon]);
-
-  useEffect(() => {
-    if (selectedPokemon) {
-      setRivalPokemon(getRivalPokemon());
-    }
-  }, [selectedPokemon]);
 
   return (
     <Grid container direction="column" spacing={2}>
